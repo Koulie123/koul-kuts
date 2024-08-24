f226ef88a163fe029845d19431ca139c34c12d82
Koulie <kouliekyriakos@hotmail.com>
2024-08-24 15:11:25 -0400

    Removed conflicts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import './style/style.css';
 
 
-=======
-<<<<<<< HEAD
-console.log('hello world');
 
-=======
 import './style/style.css';
->>>>>>> 9637e1551375bbfe517b46ee6f2fcbe847ddb9df
->>>>>>> 736988d2de21d1ef82a4c2020a521abedc548576
